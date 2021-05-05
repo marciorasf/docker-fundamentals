ce3562f9d1105148c8bcd7e08620f7dce8e03023
marciorasf <marciorasf@gmail.com>
2021-05-04 22:07:25 -0300

    add example server

---
+++ api/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+const server = app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+process.on('SIGTERM', () => {
+  server.close(() => {
+    console.log('Process terminated')
+  })
+})
